b28948584ec0fdf046b6c87b7b07534656c361c5
kntjrld <kentjarold57@gmail.com>
2025-02-23 23:10:29 +0800

    Mongo db config 1.xx

--- conn/connection.php
+++ conn/connection.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-$dotenv = Dotenv\Dotenv::createImmutable('/app/');
+$dotenv = Dotenv\Dotenv::createImmutable('/app');
 $dotenv->load();
 $mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
 // $uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=30000&socketTimeoutMS=30000';
