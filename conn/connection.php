6a4ae4bff4f0b6b3abc25583e32bae9a98f01388
kntjrld <kentjarold57@gmail.com>
2025-02-23 22:13:43 +0800

    Mongo db config 1.3

--- conn/connection.php
+++ conn/connection.php
@@ -2,7 +2,8 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=30000&socketTimeoutMS=30000';
+// $uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=30000&socketTimeoutMS=30000';
+$uri = 'mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts';
 
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri);
