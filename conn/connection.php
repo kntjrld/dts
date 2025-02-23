192635295742f4a1fa76284d0b4585a378b65540
kntjrld <kentjarold57@gmail.com>
2025-02-23 23:54:23 +0800

    Mongo db config 1.xx

--- conn/connection.php
+++ conn/connection.php
@@ -7,10 +7,17 @@ require_once __DIR__ . "/../vendor/autoload.php";
 // $dotenv->load();
 // $mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
 // $uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=30000&socketTimeoutMS=30000';
-$uri = "mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts";
+// $uri = "mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts";
 
 // Create a new client and connect to the server
-$client = new MongoDB\Client($uri);
+// $client = new MongoDB\Client($uri);
+
+use MongoDB\Driver\ServerApi;
+$uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?retryWrites=true&w=majority&appName=dts';
+// Set the version of the Stable API on the client
+$apiVersion = new ServerApi(ServerApi::V1);
+// Create a new client and connect to the server
+$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
 
 try {
     // Send a ping to confirm a successful connection
