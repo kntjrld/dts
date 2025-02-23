7ba297e2e29605415abe098e011f7406ff53ba42
kntjrld <kentjarold57@gmail.com>
2025-02-23 22:43:38 +0800

    Mongo db config 1.4

--- conn/connection.php
+++ conn/connection.php
@@ -2,8 +2,11 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
 // $uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/?ssl=true&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=30000&socketTimeoutMS=30000';
-$uri = 'mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts';
+$uri = "mongodb://dts_cluster:{$mongoDbPassword}@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts";
 
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri);
@@ -13,6 +16,6 @@ try {
     $client->selectDatabase('admin')->command(['ping' => 1]);
     // echo "Pinged your deployment. You successfully connected to MongoDB!\n";
 } catch (Exception $e) {
-    printf($e->getMessage());
+    printf($e->$mongoDbPassword);
 }
 ?>
