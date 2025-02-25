dcdb6c2e7d409861ac37333c36e74793faec7124
kntjrld <kentjarold57@gmail.com>
2025-02-25 23:11:14 +0800

    Mongo db config 1.2

--- conn/connection.php
+++ conn/connection.php
@@ -2,15 +2,24 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use Exception;
+use MongoDB\Client;
+use MongoDB\Driver\ServerApi;
+
 // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
 // $dotenv = Dotenv\Dotenv::createImmutable('/app');
 // $dotenv->load();
 // $mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
-// $uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/retryWrites=true&w=majority&appName=dts&connectTimeoutMS=60000&socketTimeoutMS=60000';
-$uri = "mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=60000&socketTimeoutMS=60000";
+$uri = 'mongodb+srv://dts_cluster:kHzNuZSPyD7fWbKD@dts.ovcm0.mongodb.net/retryWrites=true&w=majority&appName=dts&connectTimeoutMS=60000&socketTimeoutMS=60000';
+// $uri = "mongodb://dts_cluster:kHzNuZSPyD7fWbKD@dts-shard-00-00.ovcm0.mongodb.net:27017,dts-shard-00-01.ovcm0.mongodb.net:27017,dts-shard-00-02.ovcm0.mongodb.net:27017/?ssl=true&replicaSet=atlas-130y3x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dts&connectTimeoutMS=60000&socketTimeoutMS=60000";
+
+// Specify Stable API version 1
+$apiVersion = new ServerApi(ServerApi::V1);
+// Create a new client and connect to the server
+$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
 
 // Create a new client and connect to the server
-$client = new MongoDB\Client($uri);
+// $client = new MongoDB\Client($uri);
 
 try {
     // Send a ping to confirm a successful connection
