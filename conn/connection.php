64c9f0c26606e6951294b23d2a659b8b1a458526
kntjrld <kentjarold57@gmail.com>
2025-03-02 20:38:31 +0800

    Revert connection

--- conn/connection.php
+++ conn/connection.php
@@ -5,13 +5,13 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
-$dotenv->load();
-$mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
-$mongoDbUserName = $_ENV['MONGO_DB_USERNAME'];
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+// $dotenv->load();
+// $mongoDbPassword = $_ENV['MONGO_DB_PASSWORD'];
+// $mongoDbUserName = $_ENV['MONGO_DB_USERNAME'];
 
-// $mongoDbPassword = getenv('MONGO_DB_PASSWORD');
-// $mongoDbUserName = getenv('MONGO_DB_USERNAME');
+$mongoDbPassword = getenv('MONGO_DB_PASSWORD');
+$mongoDbUserName = getenv('MONGO_DB_USERNAME');
 $uri = "mongodb+srv://$mongoDbUserName:$mongoDbPassword@dts.ovcm0.mongodb.net/retryWrites=true&w=majority&appName=dts&connectTimeoutMS=60000&socketTimeoutMS=60000";
 
 // Specify Stable API version 1
